f7012e11d2fb371f32dd833293e758cd99ce148e
zakuni <kunio038@gmail.com>
2018-06-03 18:18:50 +0900

    add socket.io
    reset time to 10sec when new client is connected

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var http = require("http")
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + "/"))
 
-var server = http.createServer(app)
-server.listen(port)
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('time', function(msg){
+    console.log('time: ' + msg);
+    io.emit('time', msg);
+  });
+});
+
+http.listen(port)
 
 console.log("http server listening on %d", port)
